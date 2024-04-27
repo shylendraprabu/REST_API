2fc37ee96356383e9342ff1ff8061bd4972660c9
shylendraprabu <118248884+shylendraprabu@users.noreply.github.com>
2024-04-27 14:32:41 +0530

    Add files via upload

---
+++ example/src/main/java/example1/example/model/Model.java
@@ -0,0 +1,25 @@
+package example1.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Model
+{
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String email;
+    private String userid;
+    private String password;
+    private String conformpassword;
+    private String contentcreatername;
+    private String videosearched;
+    private String textsearched;
+}
+
+
